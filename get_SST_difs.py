86eb382bda4ee842ea7e22d95a8e92fd2964fbda
amoebaliz <liz.drenkard@gmail.com>
2019-11-27 12:59:41 -0700

    more updates to figure

--- get_SST_difs.py
+++ get_SST_difs.py
@@ -11,7 +11,6 @@ from matplotlib.collections import PolyCollection
 # NCAR directory for Large Ensemble files
 dir = '/glade/collections/cdg/data/cesmLE/CESM-CAM5-BGC-LE/ocn/proc/tseries/monthly/SST/'
 
-
 # File name strings
 his_fil_bas = 'b.e11.B20TRC5CNBDRD.f09_g16.'
 fut_fil_bas = 'b.e11.BRCP85C5CNBDRD.f09_g16.'
@@ -19,29 +18,35 @@ fut_fil_bas = 'b.e11.BRCP85C5CNBDRD.f09_g16.'
 fil_bas2 = '.pop.h.SST.'
 
 # GULF OF AK DOMAIN
+# for ncks -d nlat,313,329 -d nlon,206,238
+# in python: same start value, +1 last value
 a = 313
 b = 330
 c = 206
 d = 239
-#nlat,313,329 -d nlon,206,238
-
 nmons=1
 
 # Number of Years in climatology
-nclim_yr = 10
 nclim_yr = 30
 
 # Number of NCAR LE endmembers
 end_mem = 35
 
-# EACH MONTH (if climatology)
-for nm in range(nmons):
+# DIFS ARE FOR 30-yr CLIM DIFS
+#ne_vals = [31] # COLDEST
+#ne_vals = [13] # WARMEST
+#ne_vals = np.arange(35)+1 # ALL
 
-    # STORAGE ARRAYS FOR MONTHLY CLIM MEAN/STD
-    dif_30_all_stor = np.empty((0,b-a,d-c))
-    dif_10_all_stor = np.empty((0,b-a,d-c))
+ne_vals = [[31],[13],np.arange(35)+1]
 
-    for ne in range(1,end_mem+1):
+#dif_all_30_stor = np.empty((0,b-a,d-c))
+#dif_all_10_stor = np.empty((0,b-a,d-c))
+
+for ne_val in ne_vals:
+    dif_all_30_stor = np.empty((0,b-a,d-c))
+    dif_all_10_stor = np.empty((0,b-a,d-c))
+    for ne in ne_val:
+
         print 'Endmember #', ne
         ############################
         ## HISTORICAL CLIMATOLOGY ##
@@ -57,19 +62,16 @@ for nm in range(nmons):
 
         # Montly indicies for historical fields
         nyr_h = 2000-his_str_yr+1
-        Ih = np.arange(nm + 12*(nyr_h-nclim_yr), 12*nyr_h, nmons) 
+        Ih = np.arange(12*(nyr_h-nclim_yr), 12*nyr_h, nmons) 
 
         # Historical climatology
         sst = fid_his.variables['SST'][Ih,:,a:b,c:d].squeeze()
-        print sst.shape
-        dates = fid_his.variables['time'][Ih].squeeze()
-        print dates[0]/365,dates[0]%365,dates[-1]/365,dates[-1]%365
-        for j in range(1,9):
-            for i in range(j):
-                sst[:,j,i-1] = np.ma.masked
-
+        lats = fid_his.variables['TLAT'][a:b,c:d].squeeze()
+        #dates = fid_his.variables['time'][Ih].squeeze()
+        #print dates[0]/365,dates[0]%365,dates[-1]/365,dates[-1]%365
         SST_H_30 = np.mean(sst, axis=0) 
-        SST_H_10 = np.mean(sst[10*12:-10*12,:], axis=0) 
+        SST_H_10 = np.mean(sst[10*12:-10*12,:], axis=0)
+
         ######################## 
         ## FUTURE CLIMATOLOGY ##
         ########################
@@ -82,6 +84,7 @@ for nm in range(nmons):
         else:
            # One file: 2006-2100
            tot_yr_f = [nclim_yr]
+
         # Opening and appending variable number of files
         SST_F = np.empty((0,b-a,d-c))
         nfils = len(tot_yr_f)
@@ -91,42 +94,31 @@ for nm in range(nmons):
             # Number of years contained in future file 
             nyr_f = fut_end_yr[nf]-fut_str_yr[nf-nfils]+1
             # (Montly) indicies for future fields
-            If = np.arange(nm + int(12*(nyr_f-tot_yr_f[nf])), 12*nyr_f, nmons)
+            If = np.arange(int(12*(nyr_f-tot_yr_f[nf])), 12*nyr_f, nmons)
             # Monthly climatology/Annual Mean
             SST_F = np.ma.append(SST_F, fid_fut.variables['SST'][If,:,a:b,c:d].squeeze(), axis=0)
-            dates = fid_fut.variables['time'][If].squeeze() 
-            print dates[0]/365,dates[0]%365,dates[-1]/365,dates[-1]%365
+            #dates = fid_fut.variables['time'][If].squeeze() 
+            #print dates[0]/365,dates[0]%365,dates[-1]/365,dates[-1]%365
             # Bering Sea Masking #
-            for j in range(2,11):
-                for i in range(j-1):
-                    SST_F[:,j,i] = np.ma.masked
+            for j in range(1,9):
+                for i in range(j):
+                    SST_F[:,j,i-1] = np.ma.masked
         # Climatology
         SST_F_30 = np.mean(SST_F,axis=0)
         SST_F_10 = np.mean(SST_F[10*12:-10*12,:],axis=0)
+
         # STORE VALUES FOR ANALYSIS 
-        dif_30_all_stor = np.ma.append(dif_30_all_stor, np.ma.expand_dims(SST_F_30-SST_H_30,axis=0), axis=0)
-        dif_10_all_stor = np.ma.append(dif_10_all_stor, np.ma.expand_dims(SST_F_10-SST_H_10,axis=0), axis=0)
+        dif_all_30_stor = np.ma.append(dif_all_30_stor, np.ma.expand_dims(SST_F_30-SST_H_30,axis=0), axis=0)
+        dif_all_10_stor = np.ma.append(dif_all_10_stor, np.ma.expand_dims(SST_F_10-SST_H_10,axis=0), axis=0)
 
-    avg_dif_30_sst = np.ma.mean(np.ma.mean(dif_30_all_stor,axis=2),axis=1).squeeze()
-    avg_dif_10_sst = np.ma.mean(np.ma.mean(dif_10_all_stor,axis=2),axis=1).squeeze()
-    #print avg_dif_sst
-    ##########
-    # OUTPUT #
-    ##########
-    print "COLDEST 30yr"
-    #print 'ABS', np.argsort(avg_fut_sst)[:5]+1
-    print 'DIF', np.argsort(avg_dif_30_sst.squeeze())[:5]+1
+    # SAVE 2D FIELD FOR FIGURE GENERATION
+    if len(ne_val)==1:
+       enm_str = str(ne_val[0]).zfill(3)
+    else:
+       enm_str = 'ALL'
 
-    print "COLDEST 10yr"
-    #print 'ABS', np.argsort(avg_fut_sst)[:5]+1
-    print 'DIF', np.argsort(avg_dif_10_sst.squeeze())[:5]+1
+    filstr_30 = enm_str + '_average_' + str(30) + 'yr_dif'
+    filstr_10 = enm_str + '_average_' + str(10) + 'yr_dif'
 
-    print "WARMEST 30yr"
-    #print 'ABS', np.argsort(avg_fut_sst)[-5:]+1
-    print 'DIF', np.argsort(avg_dif_30_sst.squeeze())[-10:]+1
-
-    print "WARMEST 10yr"
-    #print 'ABS', np.argsort(avg_fut_sst)[-5:]+1
-    print 'DIF', np.argsort(avg_dif_10_sst.squeeze())[-10:]+1
-
-
+    np.mean(dif_all_30_stor,axis=0).dump(filstr_30)
+    np.mean(dif_all_10_stor,axis=0).dump(filstr_10)
--- plot_AKG.py
+++ plot_AKG.py
@@ -1,9 +1,13 @@
 import numpy as np
 import netCDF4 as nc
 import matplotlib.pyplot as plt
+import matplotlib as mpl
 import matplotlib.gridspec as gridspec
 from mpl_toolkits.basemap import Basemap
 from matplotlib.collections import PolyCollection
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+import matplotlib.dates as pltd
+import datetime as dt
 
 def polygon_patch(mapid,axs):
     mapid.drawcoastlines(linewidth=0,zorder=map_order+6)
@@ -16,59 +20,155 @@ def polygon_patch(mapid,axs):
          facecolor=(1,1,1), closed=False)
     axs.add_collection(lc)
 
+import matplotlib.pyplot as plt
+import matplotlib.colors as colors
+import numpy as np
+
+def truncate_colormap(cmap, minval=0.0, maxval=1.0, n=100):
+    new_cmap = colors.LinearSegmentedColormap.from_list(
+        'trunc({n},{a:.2f},{b:.2f})'.format(n=cmap.name, a=minval, b=maxval),
+        cmap(np.linspace(minval, maxval, n)))
+    return new_cmap
+
 def plot_map(ax):
-    # Map specs
+    # PLOT: Map object
     m = Basemap(llcrnrlat=lat[0,0]-m_off,urcrnrlat=lat[-1,0]+m_off,\
            llcrnrlon=lon[0,0]-m_off,urcrnrlon=lon[0,-1]+m_off, resolution='i')
 
-    m.drawmeridians([-160,-140], labels=[0,0,0,0], fmt='%d', fontsize=18,zorder=map_order+5)
-    m.drawparallels([55,60], labels=[0,0,0,0], fmt='%d', fontsize=18,zorder=map_order+5)
-    m.pcolormesh(pc_lon,pc_lat,pc_sst,cmap='seismic',zorder=map_order)
+    # PLOT: SST pcolormesh
+    m.pcolormesh(pc_lon,pc_lat,pc_sst,vmin=3.0,vmax=5.5,cmap=new_cmap,zorder=map_order)
     polygon_patch(m,ax)
-    ax.text(-162.5,61, str_val[n], fontsize=14)
 
+    # PLOT: Subplot ID
+    ax.text(-162.5,tx_lat, ids[n], fontsize=14)
+    # PLOT: Endmember ID
+    enm_str = 'EM ' + dat_fils[n][:3] 
+    ax.text(-137, tx_lat, enm_str, fontsize=12) 
+
+    # PLOT: Latitudes (left plots)
+    m.drawparallels([55,60], labels=[int((n+1)%2),0,0,0], fmt='%d', fontsize=12,zorder=map_order+5)
+    # PLOT: Longitudes (bottom plots)
+    m.drawmeridians([-160,-140], labels=[0,0,0,int(n/4)], fmt='%d', fontsize=12,zorder=map_order+5)
+
+    # PLOT: Titles (top plots)
+    if n<2:
+       ax.set_title(titles[n], fontsize=14)
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 
 # FIGURE
 
 m_off = 0.1
 map_order=1
-str_val = ['a','b','c','d','e','f']
-
+ids = ['a','b','c','d','e','f']
+titles = ['10 years', '30 years']
 nplot = 6
+tx_lat = 61
 
-fig = plt.figure(figsize=(11,4))
+fig = plt.figure(figsize=(11,6))
 gs = gridspec.GridSpec(3,2)
 gs.update(wspace=0.05, hspace=0.05)
 
-# Iterate over different plots
+# SETTING UP COLORMAP
+cmap = plt.get_cmap('hot_r')
+new_cmap = truncate_colormap(cmap, 0.2, 1)
+
+# LENS VALUES TO PLOT
+dat_fils = ['013_average_10yr_dif', '013_average_30yr_dif',\
+            'ALL_average_10yr_dif', 'ALL_average_30yr_dif',\
+            '031_average_10yr_dif', '031_average_30yr_dif' ] 
+
+fid = nc.Dataset('AKG_test.nc')
+lat = fid.variables['TLAT'][:]
+lon = fid.variables['TLONG'][:]
+lon[lon>180]=lon[lon>180]-360
+
+# Bilinear interpolation of lat/lon for pcolor corners
+pc_lat = (lat[:-1,:-1] + lat[1:,:-1] + lat[1:,1:] + lat[:-1,1:])/4
+pc_lon = (lon[:-1,:-1] + lon[1:,:-1] + lon[1:,1:] + lon[:-1,1:])/4
 
 for n in range(nplot):
-    fid = nc.Dataset('AKG_test.nc')
-    if n==0:
+    # OPEN: SST file
+    sst = np.load(dat_fils[n]).squeeze()
 
-       lat = fid.variables['TLAT'][:]
-       lon = fid.variables['TLONG'][:]
-       lon[lon>180]=lon[lon>180]-360
+    # MASK: Bering Sea
+    for j in range(2,11):
+        for i in range(j-1):
+            sst[j,i] = np.ma.masked
 
-       # Bilinear interpolation of lat/lon for pcolor corners
-       pc_lat = (lat[:-1,:-1] + lat[1:,:-1] + lat[1:,1:] + lat[:-1,1:])/4
-       pc_lon = (lon[:-1,:-1] + lon[1:,:-1] + lon[1:,1:] + lon[:-1,1:])/4
-
-    sst = fid.variables['SST'][0,:].squeeze()
-
-    ###########
-    # MASKING #
-    ###########
-
-    for j in range(2,10):
-        for i in range(j):
-            sst[j,i-1] = np.ma.masked
-
+    # SUBSET: For pcolor plot
     pc_sst = sst[1:-1,1:-1]
 
+    print ''
+    print 'PLOT', ids[n]
+    print np.min(pc_sst),np.max(pc_sst)
+
+    # PLOT: Maps
     ax = plt.subplot(gs[n])
     plot_map(ax)
 
+# ADJUST: plot positions for colorbar and timeline
+plt.subplots_adjust(right=.88, bottom=0.26)
+fig.canvas.draw_idle()
+
+# PLOT: Colorbar
+cb_x0 = fig.axes[nplot-1].get_position().x1 + \
+       (fig.axes[nplot-1].get_position().x0-fig.axes[0].get_position().x1)
+cb_y0 = fig.axes[nplot-1].get_position().y0
+cb_yn = fig.axes[0].get_position().y1-fig.axes[nplot-1].get_position().y0
+cax = fig.add_axes([cb_x0, cb_y0, 0.02, cb_yn])
+
+plt.colorbar(ax.collections[0],cax=cax,extend='both')  
+cax.get_yaxis().labelpad = 25
+cax.set_ylabel(r'$\Delta$ SST ($\!^\circ\!$C)', size=14,rotation=270)
+cax.tick_params(labelsize=11) 
+
+# PLOT: Timeline
+tm_x0 = fig.axes[0].get_position().x0
+
+tm_xn = fig.axes[nplot-1].get_position().x1 - \
+        fig.axes[0].get_position().x0
+
+ax2 = plt.axes([tm_x0,.1,tm_xn,.05], facecolor=(1,1,1,0)) 
+#ax2.set_title('Differential (Future-Historical) Timeframes',fontsize=14)
+ax2.set_title('Timeperiods Averaged for Differencing (Future-Historical)',fontsize=12)
+
+tim_lin = []
+for ny in np.arange(1970,2111,10):
+    tim_lin.append(pltd.date2num(dt.datetime(ny,1,1)))
+
+# PLOT: Full timeline
+ax2.plot([tim_lin[0],tim_lin[-1]],[0,0],'k')
+
+# PLOT: 30-yr timelines
+ax2.plot([pltd.date2num(dt.datetime(1971,1,1)),\
+          pltd.date2num(dt.datetime(2000,12,31))],\
+          [.2,.2],'-b',linewidth=4)
+
+ax2.plot([pltd.date2num(dt.datetime(2071,1,1)),\
+          pltd.date2num(dt.datetime(2100,12,31))],\
+          [.2,.2],'-b',linewidth=4)
+
+# PLOT: 10-yr timelines
+ax2.plot([pltd.date2num(dt.datetime(1981,1,1)),\
+          pltd.date2num(dt.datetime(1990,12,31))],\
+          [.4,.4],'-g',linewidth=4)
+
+ax2.plot([pltd.date2num(dt.datetime(2081,1,1)),\
+          pltd.date2num(dt.datetime(2090,12,31))],\
+          [.4,.4],'-g',linewidth=4)
+
+ax2.set_xticks(tim_lin)
+ax2.tick_params(axis="x", labelsize=11, labelrotation=40)
+ax2.xaxis_date()
+ax2.set_ylim(0,.7)
+ax2.set_xlim(tim_lin[0],tim_lin[-1])
+ax2.get_yaxis().set_visible(False)
+ax2.spines['top'].set_visible(False)
+ax2.spines['right'].set_visible(False)
+ax2.spines['bottom'].set_visible(False)
+ax2.spines['left'].set_visible(False)
+
+# SAVE: Figure
+plt.savefig('GoAK_aliasing_example')
 plt.show()
 
